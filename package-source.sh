4dd4ef55063150341705bb2b03c1eacae9f947b1
Bernhard Rosenkränzer <bero@lindev.ch>
2022-10-25 14:15:22 +0200

    Update

--- package-source.sh
+++ package-source.sh
@@ -8,7 +8,7 @@ rm -rf qtwebengine qtwebengine-chromium
 D="$(date +%Y%m%d)"
 git clone -b 5.15 --depth 1 git://code.qt.io/qt/qtwebengine.git
 cd qtwebengine
-git archive -o ../qtwebengine-everywhere-src-5.15.11-$D.tar --prefix qtwebengine-everywhere-src-5.15.11-$D/ origin/5.15
+git archive -o ../qtwebengine-everywhere-src-5.15.12-$D.tar --prefix qtwebengine-everywhere-src-5.15.12-$D/ origin/5.15
 cd ..
 git clone -b ${CHROMIUM}-based --depth 1 git://code.qt.io/qt/qtwebengine-chromium.git
 cd qtwebengine-chromium
